8374ac2a12928e268a4886a6b51e94b82e8045ee
jasonk19 <13520080@std.stei.itb.ac.id>
2023-01-03 22:17:09 +0700

    feat: create header and footer component

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-gray-200 text-center text-xs p-3 absolute bottom-0 w-full">
+      <h1>&copy; Copyright 2023</h1>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="border-b p-3 font-bold">
+      <h1>Rakamin Mini Project</h1>
+    </div>
+  );
+};
+
+export default Header;
