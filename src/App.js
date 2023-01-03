4725ba72aece3675559009a164ea5de8be79123f
jasonk19 <13520080@std.stei.itb.ac.id>
2023-01-03 23:08:43 +0700

    feat: create routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
+import Home from "./Views/Home";
+import About from "./Views/About";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <h1 className="text-3xl font-bold">Hello</h1>
+      <Router>
+        <Header />
+        <section className="p-3">
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
+          </Routes>
+        </section>
+      </Router>
       <Footer />
     </div>
   );
