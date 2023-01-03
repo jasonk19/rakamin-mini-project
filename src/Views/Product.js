70d401a2b891a7d4ee9c589db01610fba2bb1df6
jasonk19 <13520080@std.stei.itb.ac.id>
2023-01-03 23:57:11 +0700

    feat: add loading and error state

--- src/Views/Product.js
+++ src/Views/Product.js
@@ -1,31 +1,61 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router";
+import Loader from "../Components/Loader";
 
 const Product = () => {
   const { id } = useParams();
   const url = process.env.REACT_APP_API_URL + `products/${id}`;
-  const [product, setProduct] = useState(null);
+  const [product, setProduct] = useState({
+    loading: false,
+    data: null,
+    error: false,
+  });
 
   useEffect(() => {
     const getProduct = async () => {
-      const response = await axios.get(url);
-      setProduct(response.data);
+      setProduct({
+        loading: true,
+        data: null,
+        error: false,
+      });
+      try {
+        const response = await axios.get(url);
+        setProduct({
+          loading: false,
+          data: response.data,
+          error: false,
+        });
+      } catch (error) {
+        setProduct({
+          loading: false,
+          data: null,
+          error: true,
+        });
+      }
     };
     getProduct();
-  });
+  }, [url]);
 
   let content = null;
 
-  if (product) {
+  if (product.error) {
+    content = <p>There was an error, please refresh or try again later</p>;
+  }
+
+  if (product.loading) {
+    content = <Loader />;
+  }
+
+  if (product.data) {
     content = (
       <div>
-        <h1 className="text-2xl font-bold mb-3">{product.name}</h1>
+        <h1 className="text-2xl font-bold mb-3">{product.data.name}</h1>
         <div>
-          <img src={product.images[0].url} alt={product.name} />
+          <img src={product.data.images[0].url} alt={product.data.name} />
         </div>
-        <p className="text-xl font-bold mb-3">$ {product.price}</p>
-        <p>{product.description}</p>
+        <p className="text-xl font-bold mb-3">$ {product.data.price}</p>
+        <p>{product.data.description}</p>
       </div>
     );
   }
